398f8412d284379e254ca9a663584f9a78d34464
Ryan Running <ryanrunning@securitymetrics.com>
2017-07-18 20:58:21 -0600

    fizzbuzz project in angular.js

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,7 +1,17 @@
 class HomeController {
   constructor() {
     this.name = 'home';
+    this.numbers = [];
+    for (let i = 1; i < 101; i++) {
+      this.numbers.push(i);
+    }
   }
+
+  // need array 1-100
+  // create element for each number
+  // multiples of 3 marked & number replaced by 'Fizz'
+  // multiples of 5 marked & number replaced by 'Buzz'
+  // multiples of 3 & 5 marked & number replaced by 'FizzBuzz'
 }
 
 export default HomeController;
